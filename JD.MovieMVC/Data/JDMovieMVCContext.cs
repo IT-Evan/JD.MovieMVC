f67899e6992e6f00e059ff5d22ed297773e5e673
IT-Evan <Evan.net@163.com>
2019-08-29 17:15:42 +0800

    Initial

---
+++ JD.MovieMVC/Data/JDMovieMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace JD.MovieMVC.Models
+{
+    public class JDMovieMVCContext : DbContext
+    {
+        public JDMovieMVCContext(DbContextOptions<JDMovieMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<JD.MovieMVC.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ JD.MovieMVC/Models/MovieGenreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace JD.MovieMVC.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
